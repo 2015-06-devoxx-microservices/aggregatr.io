a6b837952036692653f949063444b426fc8b75fe
szimano <szimano@szimano.org>
2015-06-21 14:14:49 +0200

    Revert "clean for short presentation"
    This reverts commit b50827e62b56bfc530c6ac0cf5b2388917efb407.

--- src/main/java/pl/devoxx/aggregatr/aggregation/DojrzewatrUpdater.java
+++ src/main/java/pl/devoxx/aggregatr/aggregation/DojrzewatrUpdater.java
@@ -3,8 +3,13 @@ package pl.devoxx.aggregatr.aggregation;
 import com.nurkiewicz.asyncretry.RetryExecutor;
 import com.ofg.infrastructure.web.resttemplate.fluent.ServiceRestClient;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import pl.devoxx.aggregatr.aggregation.model.IngredientType;
 import pl.devoxx.aggregatr.aggregation.model.Ingredients;
+import pl.devoxx.aggregatr.aggregation.model.Version;
+
+import static com.netflix.hystrix.HystrixCommand.Setter.withGroupKey;
+import static com.netflix.hystrix.HystrixCommandGroupKey.Factory.asKey;
 
 @Slf4j
 class DojrzewatrUpdater {
@@ -42,6 +47,14 @@ class DojrzewatrUpdater {
     }
 
     private void notifyDojrzewatr(Ingredients ingredients) {
-        //TODO fill me in
+        serviceRestClient.forService("dojrzewatr")
+                .retryUsing(retryExecutor)
+                .post()
+                .withCircuitBreaker(withGroupKey(asKey("dojrzewatr_notification")))
+                .onUrl("/brew")
+                .body(ingredients)
+                .withHeaders().contentType(Version.DOJRZEWATR_V1)
+                .andExecuteFor()
+                .ignoringResponseAsync();
     }
 }
